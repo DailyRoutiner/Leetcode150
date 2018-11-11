f0aad71c471d8fa2d461d705ba83daaa96014c49
Jmac <ponmons@naver.com>
2018-11-11 23:34:41 +0900

    Hello World!를 출력하시오. 헬로우 월드

--- code1/src/code1/Code1.java
+++ code1/src/code1/Code1.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package code1;
 
 /**
@@ -11,11 +7,10 @@ package code1;
  */
 public class Code1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World!");
+        
     }
     
 }
