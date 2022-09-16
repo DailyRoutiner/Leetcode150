2ae8714fc79503e256d9a6387c5b5bc93dc394c8
SeongJun <schoi0813@conestogac.on.ca>
2022-09-16 17:30:45 -0400

    First ValidAnagram

---
+++ ValidAnagram.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+//        String s = "anagram";
+//        String t = "nagaram";
+
+        String s = "cat";
+        String t = "car";
+
+        System.out.println("result: " + isAnagram(s, t));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        boolean result = true;
+        if (s.length() != t.length()) return false;
+        
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        for (int i = 0; i < sArray.length; i++) {
+            char sChar = sArray[i];
+            char tChar = tArray[i];
+
+            if (sChar != tChar) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
