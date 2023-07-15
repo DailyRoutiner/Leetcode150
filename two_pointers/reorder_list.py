441a256e4899bb177e22eb5948d9e99edc76d37d
jun <Schoi0813@conestogac.on.ca>
2023-07-14 20:28:32 -0400

    reorder list

--- two_pointers/reorder_list.py
+++ two_pointers/reorder_list.py
@@ -5,20 +5,21 @@ class Solution:
         """
         slow = fast = head
 
+        # Step 1: Find the middle of the linked list
         while fast and fast.next:
-           slow = slow.next
+           slow = slow.next         # middle
            fast = fast.next.next
         
         second = slow.next
-
         prev = slow.next = None
-
+        # Step 2: Reverse the second half of the linked list
         while second:
             temp = second.next
             second.next = prev
             prev = second
             second = temp
 
+        # Step 3: Merge the first and reversed second half of the linked list
         first, last = head, prev
         while last:
             nxt1, nxt2 = first.next, last.next
