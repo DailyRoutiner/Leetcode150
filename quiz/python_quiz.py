92f85d5313347d495375371aadd9b1f3d88208e6
jun <Schoi0813@conestogac.on.ca>
2023-08-18 09:16:53 -0400

    Leetcode practice

---
+++ quiz/python_quiz.py
@@ -0,0 +1,17 @@
+# Quiz1
+def changeString(s):
+    
+    arr = s.split(':')    
+    res = "#".join(arr)
+
+    return res
+
+# test code:
+s = 'a:b:c:d'
+print(changeString(s))
+
+
+# Quiz 2
+a = {'A': 90, 'B': 80}
+print(a.get('C', 70)
+
