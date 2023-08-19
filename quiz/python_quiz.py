76d5998b76df3cac40a740a66cbd30468a7ad760
jun <Schoi0813@conestogac.on.ca>
2023-08-19 14:11:39 -0400

    quiz practice

--- quiz/python_quiz.py
+++ quiz/python_quiz.py
@@ -13,5 +13,37 @@ print(changeString(s))
 
 # Quiz 2
 a = {'A': 90, 'B': 80}
-print(a.get('C', 70)
+print(a.get('C', 70))
+
+
+# Quiz 3
+a = [1, 2, 3]
+a = a + [4, 5] # new list
+print(a)
+a.extend([6, 7]) # extend list
+print(a)
+
+# Quiz 4
+a = [20, 55, 67, 82, 45, 33, 90, 87, 100, 25]
+res = 0
+for i in a:
+    if i >= 50:
+        res += i
+print(res)
+
+res1 = sum([ v for v in a if v >= 50 ])
+print(res1)
+
+# Quiz 5
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    return fibonacci(n-2) + fibonacci(n-1)
+
+for i in range(10):
+    print(fibonacci(i), end=' ')
+
+# Quiz 6
+t = input().split(',')
+print(sum(map(int, t)))
 
