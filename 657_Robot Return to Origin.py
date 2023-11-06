7fe5d4e543c53223dfa4da7779e49b20d6736cf5
jun <Schoi0813@conestogac.on.ca>
2023-11-06 20:43:48 +0900

    enumerate

--- 657_Robot Return to Origin.py
+++ 657_Robot Return to Origin.py
@@ -17,3 +17,47 @@ print(Solution().judgeCircle(moves))
 
 moves = "LDRRLRUULR"
 print(Solution().judgeCircle(moves))
+
+# different solution
+# from enum import Enum 
+
+class Sol2():
+    x = y = 0
+    def up(self):
+        self.y += 1
+        return self.y 
+
+    def down(self):
+        self.y -= 1
+        return self.y 
+
+    def left(self):
+        self.x -= 1
+        return self.x
+    def right(self):
+        self.x += 1
+        return self.x
+
+    switcher = { # 상수가 아니고 function을 넣어서 복잡해짐..
+        'U': up,
+        'D': down,
+        'L': left,
+        'R': right
+    }
+
+    def switch(self, cha):
+        move_function = self.switcher.get(cha)
+        if move_function:
+            move_function(self)
+            print(f'Current x: {self.x}, Current y: {self.y}')
+        else:
+            print("Invalid move")
+
+
+sol2 = Sol2()
+moves = "RRDD"
+for i in moves:
+    sol2.switch(i)
+
+print(sol2.x)
+print(sol2.y)
